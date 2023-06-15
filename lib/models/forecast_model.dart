c1ef53d6b8ac43c7b89e50b05cc3e3eafb511484
Mayur Aqib <codewithaaqib@gmail.com>
2023-06-15 11:20:47 +0530

    first commit

--- lib/models/forecast_model.dart
+++ lib/models/forecast_model.dart
@@ -17,21 +17,4 @@ class ForeCastModel {
     this.cityName,
     this.weatherDescription,
   });
-
-  factory ForeCastModel.fromJson(Map<String, dynamic> json,
-      [Map<String, dynamic>? cityData]) {
-    return ForeCastModel(
-      date: json['dt_txt'],
-      tempMin: json['main']['temp_min'],
-      tempMax: json['main']['temp_max'],
-      humidity: json['main']['humidity'],
-      icon: json['weather'][0]['icon'],
-      cityName: cityData?['name'],
-      weatherDescription: json['weather'][0]['description'],
-    );
-  }
-  @override
-  String toString() {
-    return 'ForeCastModel(date: $date, tempMin: $tempMin, tempMax: $tempMax, humidity: $humidity, icon: $icon, cityName: $cityName, weatherDescription: $weatherDescription)';
-  }
 }
